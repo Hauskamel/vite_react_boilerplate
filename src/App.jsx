61baa164e1de7cf873a7a9e31f0c89114df27c99
Hauskamel <lukas.krojer@gmx.de>
2024-01-24 06:15:43 +0100

    working on flashcard deletable

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,33 @@ function App(props) {
 
     let [flashcards, setFlashcards] = useState(props.flashcards);
 
-    console.log(flashcards.map(card => {
-        console.log(card)
-    }))
+    function deleteFlashcard(id) {
+        const remainingFlashcards = flashcards.filter(card => id !== card.id);
+        console.log(id)
+        setFlashcards(remainingFlashcards);
+    }
 
     const flashCardsList = flashcards.map((card, index) => (
-        <Flashcard key={index} name={card.name} text={card.text} />
+        <Flashcard
+            id={card.id}
+            key={index}
+            name={card.name}
+            text={card.text}
+            deleteFlashcard={deleteFlashcard}
+        />
     ));
 
-    function addFlashcard () {
+    function addFlashcard() {
+        const newName = prompt("Type in flashcard name");
+        const newText = prompt("Type in flashcard text");
+
         const newFlashcard = {
-            name: "What ever Name",
-            text: "What ever Text"
+            name: newName,
+            text: newText
         }
         setFlashcards([...flashcards, newFlashcard])
     }
 
-    function deleteFlashcard () {
-
-    }
 
     return (
         <>
@@ -34,7 +42,6 @@ function App(props) {
 
             <div className="fc_button-wrapper">
                 <button className="fc_add-btn" onClick={addFlashcard}>Add Flashcard</button>
-                <button className="fc_remove-btn" onClick={deleteFlashcard}>Remove Flashcard</button>
             </div>
 
             <div className="fc_list_wrapper">
--- src/components/flashcard.jsx
+++ src/components/flashcard.jsx
@@ -1,10 +1,15 @@
+
+
 function Flashcard (props) {
+
 
     return (
         <>
             <div className="fc_wrapper">
-                <h1 className="fc_text">{props.text}</h1>
+                <h1 className="fc_name">{props.name}</h1>
+                <p className="fc_text">{props.text}</p>
                 <button className="fc_show-answer_btn">Show answer</button>
+                <button className="fc_remove" onClick={() => props.deleteFlashcard(props.id)}>Remove flashcard</button>
             </div>
         </>
     )
